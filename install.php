f3a361d5dc507f356b96cf613e5b9fcaa5234d3f
Ivan BOHONOSIUK <ibohonos@e1r2p14.unit.ua>
2018-04-08 18:18:51 +0300

    fix db

--- install.php
+++ install.php
@@ -47,7 +47,7 @@
 			];
 			file_put_contents("config.php", $arr, FILE_APPEND);
 			include_once "db.php";
-			$query[] = "CREATE TABLE products (
+			$query = "CREATE TABLE products (
 					id int(11) NOT NULL,
 					name varchar(255) NOT NULL,
 					description text,
@@ -56,7 +56,12 @@
 					qty int(11),
 					image varchar(255)
 				)";
-			$query[] = "CREATE TABLE users (
+			if (mysqli_query($connect, $query)) {
+				echo "New record created successfully";
+			} else {
+				echo "Error: " . $sql . "<br>" . mysqli_error($connect);
+			}
+			$query = "CREATE TABLE users (
 					id int(11) NOT NULL,
 					email varchar(255) NOT NULL,
 					pass varchar(255) NOT NULL,
@@ -64,18 +69,30 @@
 					last_name varchar(255) NOT NULL,
 					is_admin int(2) NOT NULL DEFAULT 0
 				)";
-			$query[] = "INSERT INTO users (email, pass, first_name, last_name, is_admin) VALUES
-					('admin@admin.ua', '7e77279cb4b3e9ce20b50e853e466d5af7cd63faddca227c8ef7b6d5aaece35f340c1f35e9b468bebe73c29da1057bafa2790a5ec05176f3fb07cd3d9a43cb24', 'Admin', 'Admin', 1)";
-			$query[] = "ALTER TABLE products
+			if (mysqli_query($connect, $query)) {
+				echo "New record created successfully";
+			} else {
+				echo "Error: " . $sql . "<br>" . mysqli_error($connect);
+			}
+			$query = "ALTER TABLE products
 				ADD PRIMARY KEY (id),
 				MODIFY id int(11) NOT NULL AUTO_INCREMENT";
-			$query[] = "ALTER TABLE users
+			if (mysqli_query($connect, $query)) {
+				echo "New record created successfully";
+			} else {
+				echo "Error: " . $sql . "<br>" . mysqli_error($connect);
+			}
+			$query = "ALTER TABLE users
 				ADD PRIMARY KEY (id),
 				ADD UNIQUE KEY email (email),
 				MODIFY id int(11) NOT NULL AUTO_INCREMENT";
-			$query[] = "CREATE USER 'rush00'@'localhost' IDENTIFIED BY 'rush00'";
-			// $query = mysql_query("GRANT USAGE ON *.* TO 'rush00'@'localhost'");
-			$query[] = "GRANT ALL PRIVILEGES ON rush00.* TO 'rush00'@'localhost'";
+			if (mysqli_query($connect, $query)) {
+				echo "New record created successfully";
+			} else {
+				echo "Error: " . $sql . "<br>" . mysqli_error($connect);
+			}
+			$query = "INSERT INTO users (email, pass, first_name, last_name, is_admin) VALUES
+					('admin@admin.ua', '7e77279cb4b3e9ce20b50e853e466d5af7cd63faddca227c8ef7b6d5aaece35f340c1f35e9b468bebe73c29da1057bafa2790a5ec05176f3fb07cd3d9a43cb24', 'Admin', 'Admin', 1)";
 			if (mysqli_query($connect, $query)) {
 				echo "New record created successfully";
 			} else {
